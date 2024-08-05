03aee52bae366888adea3a4702e22f1b90e9a9b9
wuyannnnnnn <603464460@qq.com>
2024-08-05 22:52:37 +0800

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up to date with 'origin/master'. # # Changes to be committed: #	modified:   perips/uart.sv #

--- perips/uart.sv
+++ perips/uart.sv
@@ -18,74 +18,72 @@ localparam  BAUD_CNT        = `CLK_FREQ / `UART_BPS;
 localparamq UART_CTRL       = 4'h0x00;
 localparamq UART_STATUS     = 4'h0x04;
 localparamq UART_BAUD       = 4'h0x08;
-localparamq UART_TX_REG     = 4'h0x0C;
-localparamq UART_RX_REG     = 4'h0x10;
+localparamq UART_TX         = 4'h0x0C;
+localparamq UART_RX         = 4'h0x10;
 
 reg [31:0]  uart_ctrl;
 reg [31:0]  uart_status;
 reg [31:0]  uart_baud;
 reg [31:0]  uart_tx_reg;
 reg [31:0]  uart_rx_reg;
+reg         tx_data_valid;
+//reg [7:0]   tx_byte_date;
+reg [7:0]   rx_byte_date;
 
+// write regs
 always_ff @( posedge clk_i ) begin
     if (!rst_n_i) begin
         uart_ctrl       <= 32'h0;
         uart_status     <= 32'h0;
         uart_baud       <= BAUD_CNT;
-        uart_rx_reg     <= 32'h0;
         uart_tx_reg     <= 32'h0;
         tx_data_valid   <= 1'b0;
     end
     else begin
         if (wr_en_i) begin
-            case (wr_addr_i[7:0])
+            priority case (wr_addr_i[7:0])
                 UART_CTRL: begin
                     uart_ctrl       <= wr_data_i;
-                    uart_status     <= 32'h0;
-                    uart_baud       <= BAUD_CNT;
-                    uart_rx_reg     <= 32'h0;
-                    uart_tx_reg     <= 32'h0;
-                    tx_data_valid   <= 1'b0;
                 end
                 
                 UART_STATUS: begin
-                    uart_ctrl       <= 32'h0;
                     uart_status[1]  <= wr_data_i[1];
-                    uart_baud       <= BAUD_CNT;
-                    uart_rx_reg     <= 32'h0;
-                    uart_tx_reg     <= 32'h0;
-                    tx_data_valid   <= 1'b0;
                 end
 
                 UART_BAUD: begin
-                    uart_ctrl       <= 32'h0;
-                    uart_status     <= 32'h0;
                     uart_baud       <= wr_data_i;
-                    uart_rx_reg     <= 32'h0;
-                    uart_tx_reg     <= 32'h0;
-                    tx_data_valid   <= 1'b0;
                 end
 
-
-
-                UART_TX_REG: begin
-                    if (uart_ctrl[0] == 1'b1 && uart_status[0] == 1'b0) begin
-                        tx_data <= wr_data_i[7:0];
-                        uart_status[0] <= 1'b1;
-                        tx_data_valid <= 1'b1;
+                UART_TX: begin
+                    if (uart_ctrl[0] && uart_status[0]) begin
+                        uart_tx_reg     <= wr_data_i;
+                        uart_status[0]  <= 1'b1;
+                        tx_data_valid   <= 1'b1;
                     end
+                end
+
+                default: begin
+                    uart_ctrl       <= uart_ctrl;
+                    uart_status     <= uart_status;
+                    uart_baud       <= uart_baud;
+                    uart_tx_reg     <= uart_tx_reg;
+                    tx_data_valid   <= tx_data_valid;
                 end
             endcase
-            end else begin
-                tx_data_valid <= 1'b0;
-                if (tx_data_ready == 1'b1) begin
-                    uart_status[0] <= 1'b0;
-                end
-                if (uart_ctrl[1] == 1'b1) begin
-                    if (rx_over == 1'b1) begin
-                        uart_status[1] <= 1'b1;
-                        uart_rx <= {24'h0, rx_data};
-                    end
+        end 
+        else begin
+            tx_data_valid   <= 1'b0;
+            uart_status[0]  <= (tx_data_ready) ? 1'b0 : uart_status[0];
+            uart_status[1]  <= (uart_ctrl[1] & rx_over) ? 1'b1 : uart_status[1];
+            uart_rx_reg     <= (uart_ctrl[1] & rx_over) ? {24'h0, rx_byte_data} : uart_rx_reg;
+        end
+    end
+end
+
+// read regs
+always_comb begin 
+    if (!rst_n_i) begin
+        rd_data_o = 32'b0;
     end
 end
 
